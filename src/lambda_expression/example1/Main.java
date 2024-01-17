335bdcb8b11594fee5d2ebcc572a2f29d667cc1a
ilaydadastan <ilayda@alpha3d.io>
2024-01-17 22:51:51 +0200

    added lambda exercises

--- src/lambda_expression/example1/Main.java
+++ src/lambda_expression/example1/Main.java
@@ -1,7 +1,24 @@
 package lambda_expression.example1;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        int sumOddNumbers = numbers.stream()
+                .filter(n -> n%2!=0)
+                .mapToInt(n -> n*n)
+                .sum();
+
+        int sumEvenNumbers = numbers.stream()
+                .filter(n -> n%2==0)
+                .mapToInt(n -> n*n)
+                .sum();
+
+        System.out.println("Sum of squares of odd numbers: " + sumOddNumbers);
+        System.out.println("Sum of squares of even numbers: " + sumEvenNumbers);
 
     }
 }
---
+++ src/lambda_expression/example2/Main.java
@@ -0,0 +1,17 @@
+package lambda_expression.example2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> keywords = Arrays.asList("glasses", "book", "notebook", "cream", "phone");
+
+        String keywordToSearch = "book";
+        Predicate<String> searchKeyword  = keyword -> keyword.equals(keywordToSearch);
+        boolean compare = keywords.stream().anyMatch(searchKeyword);
+        System.out.println(compare);
+
+    }
+}
